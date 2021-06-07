92b449ec7b8a26d3161572f151e1be31134abc09
sarahneuburger <sarahneuburger94@gmail.com>
2021-06-07 11:18:19 -0300

    Visualizar dados no carrinho ok

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importe cartService
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  // Armazenar os produtos no carrinho
+  items = this.cartService.getItems();
 
-  constructor() { }
+  // Injeção do serviço no constructor
+  constructor(private cartService: CartService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
