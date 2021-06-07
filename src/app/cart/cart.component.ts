618d8764a9acdd5fb18a5ff9afc1f483f1a1a7c0
sarahneuburger <sarahneuburger94@gmail.com>
2021-06-07 13:35:20 -0300

    Ajustes para form no cart.component.ts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-// Importe cartService
+// Import cartService
 import { CartService } from '../cart.service';
+
+// Import FormBuilder - para formulário de interação com o usuário
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -12,8 +15,25 @@ export class CartComponent implements OnInit {
   // Armazenar os produtos no carrinho
   items = this.cartService.getItems();
 
-  // Injeção do serviço no constructor
-  constructor(private cartService: CartService) {}
+  // Método para definir a  checkoutForm propriedade como um modelo de formulário contendo os campos namee address.
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+  // Injeção do serviço de carrinho no constructor
+  // Injeção do formulário no constructor. Este serviço faz parte do ReactiveFormsModule.
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  // Método para processar o formulário. Este método permite que os usuários enviem seus nomes e endereços. Além disso, esse método usa o clearCart()método de CartService para redefinir o formulário e limpar o carrinho.
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
   ngOnInit() {}
 }
