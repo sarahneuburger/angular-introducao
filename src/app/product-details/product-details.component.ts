68f37c36b451caf25e4d7ec07661fe721cdc6147
sarahneuburger <sarahneuburger94@gmail.com>
2021-06-07 10:41:49 -0300

    Ajustando rota para buscar por id

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// Importação do router para exibir informações dos products
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  // Propriedade product
+  product: Product | undefined;
 
-  constructor() { }
-
+  // Gerados automaticamente
+  // Injeção do route como parâmetro do constructor
+  // ActivatedRoute é específico para cada componente
+  constructor(private route: ActivatedRoute) {}
   ngOnInit() {
-  }
+    // Primeiro pega o id através da rota vigente
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
+    // Encontra o produto correspondente
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
 }
