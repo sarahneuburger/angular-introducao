efde39cd02a1810071c8cc9475620aae8c34dd0c
sarahneuburger <sarahneuburger94@gmail.com>
2021-06-07 11:04:40 -0300

    Vinculando addToCart a outros componentes

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 // Importação do router para exibir informações dos products
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+// Importando serviço cart
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -15,7 +17,11 @@ export class ProductDetailsComponent implements OnInit {
   // Gerados automaticamente
   // Injeção do route como parâmetro do constructor
   // ActivatedRoute é específico para cada componente
-  constructor(private route: ActivatedRoute) {}
+  // Injetando o serviço no constructor
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
   ngOnInit() {
     // Primeiro pega o id através da rota vigente
     // snapshot - contém infos da rota ativa naquele momento
@@ -24,5 +30,11 @@ export class ProductDetailsComponent implements OnInit {
 
     // Encontra o produto correspondente
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  // Implementando método de adicionar ao carrinho do serviço
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Seu produto foi adicionado ao carrinho.');
   }
 }
